f3c520be9eaaccf4e5dfa2f4299b50c1b3889a0e
Luis Restrepo <luis.restrepo7025@gmail.com>
2024-01-12 16:30:46 -0500

    Ajustar localizadores

--- src/main/java/co/com/userinterface/ProductsPage.java
+++ src/main/java/co/com/userinterface/ProductsPage.java
@@ -6,8 +6,8 @@ import net.serenitybdd.screenplay.targets.Target;
 public class ProductsPage extends PageObject {
 
     public static final Target SHOPPING_CART = Target.the("password").locatedBy("//div[@id='shopping_cart_container']");
-    static String xpathAddToCart = "(//button[@class='btn btn_primary btn_small btn_inventory'])[%s]";
-    static String xpathProductName = "(//div[@class='inventory_item_name'])[%s]";
+    static String xpathAddToCart = "(//button[@class='btn btn_primary btn_small btn_inventory '])[%s]";
+    static String xpathProductName = "(//div[@class='inventory_item_name '])[%s]";
 
     public static String addToCart(String value) {
         return String.format(xpathAddToCart, value);
